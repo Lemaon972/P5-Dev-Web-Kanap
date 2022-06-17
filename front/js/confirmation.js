623b6a500d9b3e17eec7f3a47217b0bf8b6c2186
Lemaon972 <101508613+Lemaon972@users.noreply.github.com>
2022-06-17 08:40:10 +0200

    up master

---
+++ front/js/confirmation.js
@@ -0,0 +1,9 @@
+const orderId = document.getElementById('orderId');
+const getProductId = location.search.substring(4);  
+orderId.innerHTML = getProductId;
+
+let cartData = localStorage.getItem('cart');
+let cart;
+cart = []
+console.log(cart);
+localStorage.setItem('cart', JSON.stringify(cart));
