c6d06021bbf3b8db02bda6ba54952904d321a6f2
Lemaon972 <101508613+Lemaon972@users.noreply.github.com>
2022-06-21 10:39:53 +0200

    up

--- front/js/script.js
+++ front/js/script.js
@@ -1,49 +1,68 @@
-// Fonction lancée au lancement de la page
-const init = async () => {
-    const products = await getProducts();
-    displayProducts(products);
-  };
-  
-  init();
-  
-  const result = document.querySelector("#items");
-  
-  // Récupérer les produits de l'API
-  async function getProducts() {
-    try{
-  
-      const response = await fetch("http://localhost:3000/api/products");
-      const body = await response.json();
-      return body;
-    } catch(e){
-      alert("Il y a un problème avec le serveur, merci de réessayer plus tard")
+// Défini $items pour assigner comme enfants tous les objets HTML
+
+const $items = document.getElementById("items")
+
+const ITEMS_PER_PAGE = 8
+
+// Requête FETCH API
+
+const retrieveKanapsData = async () => fetch("http://localhost:3000/api/products")
+  .then(res => res.json())
+    .then(data => data)
+    .catch(err => console.log("Le serveur à un problème", err))
+
+// Créer les autres élements HTML et leurs contenus
+
+const createKanapArticle = kanap => {
+  const $kanapArticle = document.createElement("article")
+
+  const $kanapImg = document.createElement("img")
+  $kanapImg.setAttribute("src", kanap.imageUrl)
+  $kanapImg.setAttribute("alt", kanap.altTxt)
+
+  const $kanapName = document.createElement("h3")
+  $kanapName.classList.add("productName")
+  $kanapName.innerText = kanap.name
+
+  const $kanapDescription = document.createElement("p")
+  $kanapDescription.classList.add("productDescription")
+  $kanapDescription.textContent = kanap.description
+
+  $kanapArticle.appendChild($kanapImg)
+  $kanapArticle.appendChild($kanapName)
+  $kanapArticle.appendChild($kanapDescription)
+
+  return $kanapArticle
+}
+
+// Fonction appelé par MAIN() avec kanap contenant les DATA api
+// Créer l'element A HTML avec l'ID de chaque ITEM comme URL, appelle la prochaine function
+// Et la définie comme enfant
+
+const createKanapItem = kanap => {
+  const $kanapItem = document.createElement("a")
+  $kanapItem.setAttribute("href", `./product.html?id=${kanap._id}`)
+
+  const $kanapArticle = createKanapArticle(kanap)
+
+  $kanapItem.appendChild($kanapArticle)
+
+  return $kanapItem
+}
+
+// récupère les data de l'API dans kanapsData
+// Boucle FOR pour chaque ITEM =>
+// Appelle createKanapItem pour créer les objets HTML en passant le contenu de l'API
+// Défini comme enfant de $items
+
+  const main = async () => {
+    const kanapsData = await retrieveKanapsData()
+
+    for (let i = 0; i < ITEMS_PER_PAGE; i++) {
+        if (kanapsData[i]) {
+            $items.appendChild(createKanapItem(kanapsData[i]))
+        }
     }
   }
-  
-  
-  
-  // Afficher les produits
-  function displayProducts(products) {
-    for (const product of products) {
-      const a = document.createElement("a");
-      const article = document.createElement("article");
-      const img = document.createElement("img");
-      const h3 = document.createElement("h3");
-      const p = document.createElement("p");
-  
-      h3.textContent = `${product.name}`;
-      p.textContent = `${product.description}`;
-  
-      a.setAttribute("href", `./product.html?id=${product._id}`);
-      img.setAttribute("src", `${product.imageUrl}`);
-      img.setAttribute("alt", `${product.altTxt}`);
-      h3.setAttribute("class", `${product.name}`);
-      p.setAttribute("class", `${product.description}`);
-  
-      result.appendChild(a);
-      a.appendChild(article);
-      article.appendChild(img);
-      article.appendChild(h3);
-      article.appendChild(p);
-    }
-  }
+
+main()
